53c526828d902957f8cc88029bad850b9276fa62
MossFrog <kaanozcelik@sabanciuniv.edu>
2016-04-05 22:30:37 +0200

    Updated the KATCE template.

--- KATCE/Katce-Template.asm
+++ KATCE/Katce-Template.asm
@@ -6,6 +6,7 @@ SPACE EQU $20
 
 main:
 	
+	*--- Output the string three times.
 	LEA INSTR,A1
 	MOVE.W	#$0D,-(SP)
 	TRAP #4
@@ -23,6 +24,8 @@ LOOP:
 	RTS
 	
 	*--- All Constants go here
+	*--- Note that KATCE does not operate on C-Type Strings.
+	*--- Thus no null terminator is required and the string length must precede the string.
 INSTR: DC.B 7,"HELLO",CR,LF
 	
 	END
